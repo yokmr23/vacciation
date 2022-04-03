94a0ff99b0036767440509d349052d496a065713
yokmr23 <56705576+yokmr23@users.noreply.github.com>
2022-04-03 11:28:38 +0900

    1週間追加

--- virus.py
+++ virus.py
@@ -19,7 +19,7 @@ from PySide6.QtWidgets import (
     QPushButton,
     QSlider,
 )
-from matplotlib.dates import DayLocator
+from matplotlib.dates import DayLocator, WeekdayLocator
 
 df = pd.read_csv(
     'https://covid19.mhlw.go.jp/public/opendata/'
@@ -38,10 +38,11 @@ class ToolPanel(QWidget):
         self.group = QButtonGroup()
         gb = QGridLayout()
         data = [
-            (0, 0, 0, "１ヶ月"),
-            (1, 0, 1, "３ヶ月"),
-            (2, 0, 2, "１年"),
-            (3, 0, 3, "全て")
+            (0, 0, 0, "1週間"),
+            (1, 0, 1, "１ヶ月"),
+            (2, 0, 2, "３ヶ月"),
+            (3, 0, 3, "１年"),
+            (4, 0, 4, "全て")
         ]
         for x, y, id, s in data:
             button = QPushButton(s, self)
@@ -73,31 +74,40 @@ class PlotWidget(QWidget):
         self.slider.setMinimum(100)
         self.slider.setMinimum(100)
         self.slider.setValue(100)
-        self.slider.setSingleStep(3)
+        self.slider.setSingleStep(4)
         # layout = QVBoxLayout()
         self.combobox = QComboBox(self)
         # self.btn1 = QPushButton('OK')
         self.label = QLabel(self)
-        self.label1 = QLabel(self)
-        self.label2 = QLabel(self)
+        self.label1 = QLabel(self)  # 新規感染者数表示
+        self.label2 = QLabel(self)  # 一週間平均
+        self.label3 = QLabel(self)  # 前週平均
+        self.label4 = QLabel(self)  # 前回比表示
+        self.label5 = QLabel(self)  # 前週平均比
         # create layout
         hlayout = QHBoxLayout()
-        hlayout.addWidget(self.combobox)
-        # layout.addWidget(self.btn1)
+        hlayout1 = QHBoxLayout()
+        hlayout2 = QHBoxLayout()
+        hlayout.addWidget(self.combobox)    # 都道府県名設定
         hlayout.addWidget(self.label)
-
+        hlayout1.addWidget(self.label1)
+        hlayout1.addWidget(self.label2)
+        hlayout1.addWidget(self.label3)
+        hlayout2.addWidget(self.label4)
+        hlayout2.addWidget(self.label5)
         vlayout = QVBoxLayout()
         vlayout.addWidget(self.toolbar)
         vlayout.addWidget(self.toolpanel)
-        vlayout.addWidget(self.label1)
-        vlayout.addWidget(self.label2)
+        # vlayout.addWidget(self.label1)
+        vlayout.addLayout(hlayout1)
+        vlayout.addLayout(hlayout2)
         # vlayout.addWidget(self.view)
         vlayout.addWidget(self.canvas)
         vlayout.addWidget(self.slider)
         vlayout.addLayout(hlayout)
         self.setLayout(vlayout)
 
-        self.combobox.setEditable(True)
+        self.combobox.setEditable(False)
         # コンボボックスの選択肢を追加
         self.draw1('ALL', '全国')
         self.combobox.addItems(dpref['都道府県名'])
@@ -109,7 +119,7 @@ class PlotWidget(QWidget):
     def changeButton(self):
         # self.slider.setValue = 100
         id = self.toolpanel.get_id()
-        if id == 3:
+        if id == 4:
             self.slider.setMaximum(100)
             self.slider.setMinimum(100)
         else:
@@ -134,48 +144,91 @@ class PlotWidget(QWidget):
         # print(id, slider_value)
         self.label.setText(pref_alphabet)
         # print(df[pref_alphabet])
-        # x = df['Date']
-        # y = df[pref_alphabet]
-        y0 = df.at[len(df)-1, pref_alphabet]
-        y1 = df.at[len(df)-2, pref_alphabet]
+        n = len(df)-1   # 最終行のindexを求める
+        # 最新の感染者数を求める
+        y0 = df.at[n, pref_alphabet]
+        # 前回の感染者数を求める
+        y1 = df.at[n-1, pref_alphabet]
         if y0 > y1:
             str0 = f'↑ {y0-y1:,}'
         else:
             if y0 == y1:
                 str0 = '変化なし'
             else:
-                str0 = f'↓ {y1-y0:,}'
+                str0 = f'↓ {y0-y1:,}'
 
         self.label1.setText(f'新規感染者数　{y0:,}人')
-        self.label2.setText(f'前回比　{str0:}人')
+        self.label4.setText(f'前回比　{str0:}人')
+        # 今週の感染者数の一日あたりの平均値(四捨五入)を求める
+        ave_one_week = int(
+            df.loc[n-6:n, pref_alphabet].mean()+0.5)
+        # 前週の感染者数の一日あたりの平均値(四捨五入)を求める
+        ave_before_week = int(
+            df.loc[n-13:n-7, pref_alphabet].mean()+0.5)
+        if ave_one_week > ave_before_week:
+            str0 = f'↑ {ave_one_week-ave_before_week:,}'
+        else:
+            if ave_one_week == ave_before_week:
+                str0 == '変化なし'
+            else:
+                str0 = f'↓ {ave_one_week-ave_before_week:,}'
+        self.label2.setText(f'一週間の平均　{ave_one_week:,}人/日')
+        self.label3.setText(f'前週の平均　{ave_before_week:,}人/日')
+        self.label5.setText(f'前週平均比 {str0}人/日')
         # print(y0, y1)
         self.ax.cla()
         self.ax.yaxis.set_major_formatter(lambda x, pos=None: f'{int(x):,}')
-        formatter = mdates.DateFormatter('%y-%m-%d')
-        self.ax.xaxis.set_major_formatter(formatter)
-        s = pd.Series([1, 2, 4, 8], index=[0, 1, 2, 3])
-        self.ax.xaxis.set_major_locator(mdates.WeekdayLocator(
-            byweekday=SU, interval=s[id], tz=None))
-        self.ax.xaxis.set_minor_locator(DayLocator(interval=s[id]))
+        # formatter = mdates.DateFormatter('%y-%m-%d')
+        self.ax.xaxis.set_major_formatter(mdates.DateFormatter('%y-%m-%d'))
+        match id:
+            case 0:
+                self.ax.xaxis.set_major_locator(DayLocator())
+            case 1:
+                self.ax.xaxis.set_major_locator(WeekdayLocator(
+                    byweekday=SU, tz=None))
+                self.ax.xaxis.set_minor_locator(DayLocator())
+            case 2:
+                self.ax.xaxis.set_major_locator(WeekdayLocator(
+                    byweekday=SU, interval=2, tz=None))
+                self.ax.xaxis.set_minor_locator(DayLocator())
+            case 3:
+                self.ax.xaxis.set_major_locator(mdates.MonthLocator(
+                    bymonthday=1, interval=2))
+                self.ax.xaxis.set_minor_locator(mdates.MonthLocator(
+                    bymonthday=1
+                ))
+            case 4:
+                self.ax.xaxis.set_major_locator(mdates.MonthLocator(
+                    bymonthday=1, interval=4))
+                self.ax.xaxis.set_minor_locator(mdates.MonthLocator(
+                    bymonthday=1
+                ))
         self.ax.tick_params(axis='x', labelrotation=45)
         label0 = self.ax.xaxis.get_majorticklabels()
         for item in label0:
             item.set_ha('right')
 
-        self.ax.set_title(f'日々の感染者数({prefecture:s})')
+        self.ax.set_title(f'日々の感染者数({prefecture})')
         self.ax.set_ylabel('感染者数')
+        # グラフ表示データ範囲を求める
         xmax, xmin = self.get_range(len(df), id, slider_value)
         x = df.iloc[xmin:xmax]
         x = x['Date']
         y = df.iloc[xmin:xmax]
         y = y[pref_alphabet]
-        self.ax.bar(x, y)
+        self.ax.bar(x, y, zorder=3)
+        if id == 0:
+            for i in range(xmin, xmax):
+                self.ax.annotate(
+                    f'{y.loc[i]:,}',
+                    xy=(x.loc[i], y.loc[i]),
+                    ha='center')
         self.ax.grid()
         self.canvas.draw()
 
     def get_range(self, length, id, v):
-        if id < 3:
-            s = pd.Series([30, 90, 365], index=[0, 1, 2])
+        if id < 4:
+            s = pd.Series([7, 30, 90, 365], index=[0, 1, 2, 3])
             s = s[id]
             xmax = int((length-s)*v/100+s)
             xmin = xmax - s
